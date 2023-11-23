41a51a940dff1495de8ace335bb048c5d1bd3a7a
Aljadis Tavera <aljadis.tavera@globant.com>
2023-11-23 12:06:25 -0500

    Fomatting

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,50 +1,66 @@
-'use strict'
+"use strict";
 
-const { postSchema, patchSchema } = require('../../schemas/book-schema.js');
-const { ObjectId } = require('@fastify/mongodb');
+const { postSchema, patchSchema } = require("../../schemas/book-schema.js");
+const { ObjectId } = require("@fastify/mongodb");
 
 module.exports = async function (fastify, opts) {
-  const collection = fastify.mongo.db.collection('books');
+  const collection = fastify.mongo.db.collection("books");
 
-  fastify.get('/books', { onRequest: [fastify.authenticate] }, async function (request, reply) {
-    console.log(request.user); // Access the user data
-    const result = await collection.find().toArray();
-    return result;
-  })
+  fastify.get(
+    "/books",
+    { onRequest: [fastify.authenticate] },
+    async function (request, reply) {
+      console.log(request.user); // Access the user data
+      const result = await collection.find().toArray();
+      return result;
+    }
+  );
 
-  fastify.get('/books/:id', async function (req, rep) {
+  fastify.get("/books/:id", async function (req, rep) {
     const result = await collection.findOne({
       _id: new ObjectId(req.params.id),
     });
     return result;
-  })
+  });
 
-  fastify.post('/books', { schema: postSchema }, async function (req, rep) {
-    const book = req.body;
-    const result = await collection.insertOne(book);
-    return { _id: result.insertedId };
-  })
+  fastify.post(
+    "/books",
+    { schema: postSchema, onRequest: [fastify.authenticate] },
+    async function (req, rep) {
+      const book = req.body;
+      const result = await collection.insertOne(book);
+      return { _id: result.insertedId };
+    }
+  );
 
-  fastify.delete('/books/:id', async function (req, rep) {
-    const result = await collection.deleteOne({
-      _id: new ObjectId(req.params.id),
-    });
-    return result.deletedCount > 0;
-  })
-
-  fastify.patch('/books/:id', { schema: patchSchema }, async function (req, rep) {
-    const result = await collection.findOneAndUpdate(
-      {
+  fastify.delete(
+    "/books/:id",
+    { onRequest: [fastify.authenticate] },
+    async function (req, rep) {
+      const result = await collection.deleteOne({
         _id: new ObjectId(req.params.id),
-      },
-      {
-        $set: { ...req.body },
-      },
-      {
-        returnNewDocument: true,
-        returnDocument: "after",
-      }
-    );
-    return result;
-  })
-}
+      });
+      return result.deletedCount > 0;
+    }
+  );
+
+  fastify.patch(
+    "/books/:id",
+    { schema: patchSchema, onRequest: [fastify.authenticate] },
+    async function (req, rep) {
+      const result = await collection.findOneAndUpdate(
+        {
+          _id: new ObjectId(req.params.id),
+        },
+        {
+          $set: { ...req.body },
+        },
+        {
+          returnNewDocument: true,
+          returnDocument: "after",
+        }
+      );
+      return result;
+    }
+  );
+};
